ca1fffe716bf60a2669e7407cb7762e2e2c7bc8d
dibya0007 <46745664+dibya0007@users.noreply.github.com>
2020-10-16 10:55:44 +0530

    Create test.java

---
+++ src/com/lti/model/test.java
@@ -0,0 +1,18 @@
+package com.lti.model;
+
+public class test {
+	int tid;
+	String tname;
+	public int getTid() {
+		return tid;
+	}
+	public void setTid(int tid) {
+		this.tid = tid;
+	}
+	public String getTname() {
+		return tname;
+	}
+	public void setTname(String pname) {
+		this.tname = tname;
+	}
+}
