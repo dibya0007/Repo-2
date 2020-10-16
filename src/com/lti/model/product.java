e47b5587a9da66e94b835d675e50a8835c89f5a4
dibya0007 <46745664+dibya0007@users.noreply.github.com>
2020-10-16 10:49:08 +0530

    back to old form product.java

--- src/com/lti/model/product.java
+++ src/com/lti/model/product.java
@@ -2,14 +2,18 @@ package com.lti.model;
 
 public class product {
 	int pid;
-	
-
+	String pname;
 	public int getPid() {
 		return pid;
 	}
-	
 	public void setPid(int pid) {
 		this.pid = pid;
 	}
-	
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
 }
+
